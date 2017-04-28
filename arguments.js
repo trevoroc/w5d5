ae0dec7b6b866a2d351497d8c81d22e8a862a0ad
App Academy Student <student@appacademy.io>
2017-04-28 13:48:40 -0700

    Finish myBinds

--- arguments.js
+++ arguments.js
@@ -18,3 +18,37 @@ function sumRest(...args) {
 
   return total;
 }
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  says(sound, person) {
+    console.log(`${this.name} says ${sound} to ${person}!`);
+    return true;
+  }
+}
+
+const markov = new Cat("Markov");
+const breakfast = new Cat("Breakfast");
+
+Function.prototype.myBind = function (context) {
+  let argsArr = Array.from(arguments);
+  // let ctx = context;
+  // console.log(argsArr);
+  let func = this;
+
+  return function () {
+    let argsArr2 = Array.from(arguments);
+    console.log(argsArr);
+    console.log(argsArr2);
+    func.apply(context, argsArr.slice(1).concat(argsArr2));
+  };
+};
+
+Function.prototype.myBindRest = function (context, ...args) {
+  return (...args2) => {
+    this.apply(context, args.concat(args2));
+  };
+};
